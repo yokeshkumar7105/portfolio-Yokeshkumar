32921e5b889571ce742222891ec42dc07daef3b6
yokeshkumar7105 <yokeshkumarcse@gmail.com>
2024-12-02 22:46:12 +0530

    updated main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import '/src/index.css'
+import './main.jsx';  
+
 import App from './App.jsx';
 
 import 'lenis/dist/lenis.css';
