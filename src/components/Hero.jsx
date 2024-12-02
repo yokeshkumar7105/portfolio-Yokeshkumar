21dd3a8f465ccc2cb44af9cddc662c9356514e0b
yokeshkumar7105 <yokeshkumarcse@gmail.com>
2024-12-03 00:08:10 +0530

    updated package.json

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -56,7 +56,7 @@ const Hero = () => {
         <div className="hidden lg:block">
           <figure className="w-full max-w-[480px] ml-auto bg-gradient-to-t from-lime-400 via-25% via-lime-400/40 to-65% rounded-[60px] overflow-hidden">
             <img 
-            src="/images/herobanner.png" 
+            src="/portfolio-Yokeshkumar/images/herobanner.png" 
             width={656}
             height={800}
             alt="Yokeshkumar" 
