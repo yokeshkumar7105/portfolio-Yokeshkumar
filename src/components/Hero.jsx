407a9feacccca6d2d57a48b8322b0961ab94b26a
yokeshkumar7105 <yokeshkumarcse@gmail.com>
2024-12-03 00:01:13 +0530

    updated package.json

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
           <div className="flex items-center gap-3">
             <figure className="img-box w-9 h-9 rounded-lg">
               <img 
-              src="/images/avatar-1.jpg" 
+              src="/portfolio-Yokeshkumar/images/avatar-1.jpg" 
               width={40}
               height={40}
               alt="Yokeshkumar" 
