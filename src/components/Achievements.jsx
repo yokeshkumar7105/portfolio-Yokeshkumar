10b361a332d76b596785cd7ca93925d17ff39f46
yokeshkumar7105 <yokeshkumarcse@gmail.com>
2024-12-03 00:29:38 +0530

    updated package.json

--- src/components/Achievements.jsx
+++ src/components/Achievements.jsx
@@ -24,37 +24,37 @@ const achievements = [
   {
     content: 'Participated in college Ideathon and demonstrated our idea to deploy in real world..',
     name: 'Ideathon 2022',
-    imgSrc: '/images/achievement-1.png',
+    imgSrc: '/portfolio-Yokeshkumar/images/achievement-1.png',
     company: 'Ideathon'
   },
   {
     content: 'Participated in Smart India Hackathon and demonstrated our idea to contribute to real world government projects.',
     name: 'Smart India Hackathon',
-    imgSrc: '/images/achievement-1.png',
+    imgSrc: '/portfolio-Yokeshkumar/images/achievement-1.png',
     company: 'Hackathon'
   },
    {
     content: 'Learnt about Machine Learning through Cognizant\'s virtual Internship through Forage.',
     name: 'Cognizant',
-    imgSrc: '/images/achievement-2.jpg',
+    imgSrc: '/portfolio-Yokeshkumar/images/achievement-2.jpg',
     company: 'Virtual Intern'
   },
   {
     content: 'Participated in AI hackathon orgainized by SRM university and hacked the leap day of 2024.',
     name: 'Leap\'24',
-    imgSrc: '/images/achievement-1.png',
+    imgSrc: '/portfolio-Yokeshkumar/images/achievement-1.png',
     company: 'Hackathon'
   },
   {
     content: 'Designed posters for clients and Collaborated with internal teams on project briefs to ensure timely delivery of projects within budget constraints.',
     name: 'Critter Graphix',
-    imgSrc: '/images/achievement-3.png',
+    imgSrc: '/portfolio-Yokeshkumar/images/achievement-3.png',
     company: 'Design Intern'
   },
   {
     content: 'Developed webpages using different frontend technologies through this educational internship and gained practical experience.',
     name: 'CognoRise Infotech',
-    imgSrc: '/images/achievement-4.png',
+    imgSrc: '/portfolio-Yokeshkumar/images/achievement-4.png',
     company: 'Intern'
   },
 ];
