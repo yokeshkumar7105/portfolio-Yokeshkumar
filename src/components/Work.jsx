523f4e4e4e8fc01b9e93c0b93ff2a09d27943c11
yokeshkumar7105 <yokeshkumarcse@gmail.com>
2024-12-03 00:25:36 +0530

    updated package.json

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -6,37 +6,37 @@ import ProjectCard from "./ProjectCard";
 
 const works = [
   {
-    imgSrc: '/images/project-1.png',
+    imgSrc: '/portfolio-Yokeshkumar/images/project-1.png',
     title: 'Chennai Metro Path Finder',
     tags: ['HTML', 'CSS', 'JS'],
     projectLink: 'https://github.com/yokeshkumar7105/Projects/tree/main/metroTrackingSystem'
   },
   {
-    imgSrc: '/images/project-2.jpg',
+    imgSrc: '/portfolio-Yokeshkumar/images/project-2.jpg',
     title: 'Free stock photo app',
     tags: ['API', 'SPA'],
     projectLink: 'https://pixstock-official.vercel.app/'
   },
   {
-    imgSrc: '/images/project-3.jpg',
+    imgSrc: '/portfolio-Yokeshkumar/images/project-3.jpg',
     title: 'Recipe app',
     tags: ['Development', 'API'],
     projectLink: ''
   },
   {
-    imgSrc: '/images/project-4.png',
+    imgSrc: '/portfolio-Yokeshkumar/images/project-4.png',
     title: 'Streamverse: An OTT Application',
     tags: ['HTML','CSS', 'Frontend'],
     projectLink: 'https://github.com/yokeshkumar7105/Projects/tree/main/streamverse'
   },
   {
-    imgSrc: '/images/project-5.jpg',
+    imgSrc: '/portfolio-Yokeshkumar/images/project-5.jpg',
     title: 'eCommerce website',
     tags: ['eCommerce', 'Development'],
     projectLink: ''
   },
   {
-    imgSrc: '/images/project-6.png',
+    imgSrc: '/portfolio-Yokeshkumar/images/project-6.png',
     title: 'Personal portfolio',
     tags: ['REACT', 'TAILWIND'],
     projectLink: '/'
