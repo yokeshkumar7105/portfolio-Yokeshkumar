f293590ea06a518dc8f7d9e77fdd0022f97e9275
yokeshkumar7105 <yokeshkumarcse@gmail.com>
2024-12-02 23:47:07 +0530

    updated Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,7 +13,7 @@ const Header = ()=>{
             <div className="max-w-screen-2xl w-full mx-auto px-4 flex justify-between items-center md:px-6 md:grid md:grid-cols-[1fr,3fr,1fr]">
                 <h1>
                     <a 
-                    href="/" 
+                    href="/portfolio-Yokeshkumar/" 
                     className="logo"
                     >
                         <img 
